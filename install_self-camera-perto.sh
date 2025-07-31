d880659bf471bc1595c35b1d0a8a6a2024d6b53b
elppans <elppansmk@gmail.com>
2025-07-31 15:18:09 -0300

    Update install_self-camera-perto.sh

--- install_self-camera-perto.sh
+++ install_self-camera-perto.sh
@@ -81,6 +81,9 @@ rm -rf /Zanthus/Zeus/pdvJava/PDVTouch.sh
 cp /root/PDVTouch.sh /Zanthus/Zeus/pdvJava/PDVTouch.sh
 rm /root/PDVTouch.sh
 
+# https://raw.githubusercontent.com/elppans/pdv-touch/refs/heads/main/PDVTouch_Simples_Perto.sh
+# https://raw.githubusercontent.com/elppans/pdv-touch/refs/heads/main/x11vnc.sh
+
 echo "[5] - Concedendo permissoes:"
 chmod +x /Zanthus/Zeus/pdvJava/PDVTouch.sh
 chmod +x /root/self_perto_config.sh
