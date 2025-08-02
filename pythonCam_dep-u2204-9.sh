b545fe04056ce7466926d09eb305d9e3a871e5d4
elppans <elppans@gmail.com>
2025-08-02 02:36:24 -0300

    Atualizado deb.sh

--- pythonCam_dep-u2204-9.sh
+++ pythonCam_dep-u2204-9.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+# shellcheck disable=SC2316
+
+local="$(pwd)"
+aptcache="/var/cache/apt/archives"
+export local
+export aptcache
+export APT_CONFIG=/dev/null
 
 # apt --fix-broken install
-apt-get -f install
+sudo apt-get -f install
 
-cd pythonCam_dep-u2204-9
-sudo apt install ./*.deb
+cp -a "$local"/pythonCam_dep-u2204-9 "$aptcache"
+sudo apt install "$aptcache"/*.deb
 
